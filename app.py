0cc6baa01d7decf97451c8d50078462b4c64adff
Mostafa Nabil <60539423+MostafaNabilll@users.noreply.github.com>
2022-10-15 19:11:21 +0200

    update path

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ import os
 
 absolute_path = os.path.dirname(__file__)
 
-
 full_path = os.path.join(absolute_path, "Notebook")
 
-st.set_option('deprecation.showfileUploaderEncoding',False)
-@st.cache(allow_output_mutation=True)
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
+
+@st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model(full_path +'\my_model.hdf5',custom_objects={'KerasLayer':hub.KerasLayer})
+    model = tf.keras.models.load_model(full_path + '\my_model.hdf5', custom_objects={'KerasLayer': hub.KerasLayer})
     return model
+
 
 model = load_model()
 st.title("""
@@ -23,15 +24,15 @@ st.title("""
 """)
 st.subheader("Choose a Image with Cat or Dog")
 
-file = st.file_uploader("Upload your Image",type=['jpg','png'])
+file = st.file_uploader("Upload your Image", type=['jpg', 'png'])
 
-def import_and_predict(image_data,model):
-    
-    size = (224,224)
-    image = ImageOps.fit(image_data,size,Image.ANTIALIAS)
+
+def import_and_predict(image_data, model):
+    size = (224, 224)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
     img = np.array(image)
     img_reshape = img[np.newaxis]
-    img_scale = img_reshape/255
+    img_scale = img_reshape / 255
     prediction = model.predict(img_scale)
     return prediction
 
@@ -40,8 +41,8 @@ st.markdown("<h4 style='text-align:left; color:gray'> Prediction </h4>", unsafe_
 
 if file is not None:
     image = Image.open(file)
-    st.image(image,width=None)
-    predictions = import_and_predict(image,model)
-    class_names=["Cat",'Dog']
+    st.image(image, width=None)
+    predictions = import_and_predict(image, model)
+    class_names = ["Cat", 'Dog']
     string = "What you see here is a " + class_names[np.argmax(predictions)]
     st.success(string)
